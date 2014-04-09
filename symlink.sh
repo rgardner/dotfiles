556be01b49aae55e1c60deed2a4cf9a28a8e28bd
Robert Gardner <rhg259@nyu.edu>
2014-04-09 19:47:08 -0400

    Remove .vim/ from symlink script and refactor into loop

--- symlink.sh
+++ symlink.sh
@@ -4,36 +4,33 @@
 printf "This script will permanently delete the following dotfiles from your "
 printf "home directory and replace them with symlinks to the corresponding "
 printf "files in this directory:"
-declare -a files=(".bash_profile" ".bashrc" ".gemrc" ".gitconfig"
-                  ".gitignore_global" ".gitk" ".npmrc" ".railsrc" ".redebugrc"
-                  ".vim/" ".vimrc")
+declare -a files=(".bash_profile"     ".bashrc" ".gemrc" ".gitconfig"
+                  ".gitignore_global" ".gitk"   ".npmrc" ".railsrc" ".rdebugrc"
+                  ".vimrc")
 echo
 for i in "${files[@]}"
 do
-  echo "  $i"
+  printf "\t%s\n" "$i"
 done
 
-read -p "Are you sure? " -n 1 -r
+read -p "Do you wish to proceed? [y/n] " -n 1 -r
 echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
+  printf "No symlinks created.\n"
   exit 1
 fi
 
 
 dotfiles=$(pwd)
 # Symlink dotfiles to home directory
-printf "Making Terminal symlinks...\n"
-cd ~; rm .bash_profile; ln -s $dotfiles/.bash_profile .bash_profile
-cd ~; rm .bashrc;       ln -s $dotfiles/.bashrc       .bashrc
-cd ~; rm .gemrc;        ln -s $dotfiles/.gemrc        .gemrc
-cd ~; rm .gitconfig;    ln -s $dotfiles/.gitconfig    .gitconfig
-cd ~; rm .gitignore_global; ln -s $dotfiles/.gitignore_global .gitignore_global
-cd ~; rm .gitk;         ln -s $dotfiles/.gitk         .gitk
-cd ~; rm .npmrc;        ln -s $dotfiles/.npmrc        .npmrc
-cd ~; rm .railsrc;      ln -s $dotfiles/.railsrc      .railsrc
-cd ~; rm .redebugrc;    ln -s $dotfiles/.redebugrc    .redebugrc
-cd ~; rm -rf .vim/;     ln -s $dotfiles/.vim/         .vim/
-cd ~; rm .vimrc;        ln -s $dotfiles/.vimrc        .vimrc
+printf "Making Terminal symlinks in %s\n" "$HOME"
+cd ~
+for i in "${files[@]}"
+do
+  printf "\tSymlinking %s\n" "$i"
+  rm "$i"
+  ln -s "$dotfiles"/"$i" "$i"
+done
 
-printf "\nFinished!\n"
+printf "Finished!\n"
