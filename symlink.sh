7fec3faca9492fae610d629ddd431a40715da593
Robert Gardner <rhg259@nyu.edu>
2014-08-11 20:45:46 -0400

    Add .ssh and .profile to symlinks

--- symlink.sh
+++ symlink.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
+#
+# Install symlinks in home directory to their corresponding dotfiles here.
 
 # Confirm the user wants to remove all dotfiles in their home directory
 printf "This script will permanently delete the following dotfiles from your "
 printf "home directory and replace them with symlinks to the corresponding "
 printf "files in this directory:"
 declare -a files=(".bash_profile"     ".bashrc" ".gemrc" ".gitconfig"
-                  ".gitignore_global" ".gitk"   ".npmrc" ".railsrc" ".rdebugrc"
-                  ".vimrc")
+                  ".gitignore_global" ".gitk"   ".npmrc" ".profile" ".railsrc"
+                  ".rdebugrc" ".ssh-auto-completion.sh" ".vimrc")
 echo
 for i in "${files[@]}"
 do
