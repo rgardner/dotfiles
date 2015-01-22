1d8a2a188cf6ebac40adc245f5ce9e6683b92696
Robert Gardner <rhg259@nyu.edu>
2015-01-21 23:45:37 -0500

    Small ubuntu apt file

---
+++ init/20_ubuntu_apt.sh
@@ -0,0 +1,24 @@
+# Ubuntu-only stuff. Abort if not Ubuntu.
+is_ubuntu || return 1
+
+# Update APT.
+e_header "Updating APT"
+sudo apt-get -qq update
+sudo apt-get -qq dist-upgrade
+
+# Install APT packages.
+packages=(
+  build-essential
+  git-core
+  htop
+  tree
+)
+
+packages=($(setdiff "${packages[*]}" "$(dpkg --get-selections | grep -v deinstall | awk '{print $1}')"))
+
+if (( ${#packages[@]} > 0 )); then
+  e_header "Installing APT packages: ${packages[*]}"
+  for package in "${packages[@]}"; do
+    sudo apt-get -qq install "$package"
+  done
+fi
