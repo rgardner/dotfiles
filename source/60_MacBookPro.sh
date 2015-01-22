017484968b5bc3ee7be785818379e1d490753c75
Robert Gardner <rhg259@nyu.edu>
2015-01-21 22:48:23 -0500

    Move Darwin and MacBookPro

--- source/60_MacBookPro.sh
+++ source/60_MacBookPro.sh
@@ -25,8 +25,9 @@ export NVM_DIR="/Users/bobgardner/.nvm"
 export PATH="$HOME/.rvm/bin:$PATH" # Add RVM to PATH for scripting
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
+# Autojump support.
+[[ -s "$(brew --prefix)"/etc/autojump.sh ]] && . "$(brew --prefix)"/etc/autojump.sh
+
 # Aliases
 alias arduino='/Applications/Arduino.app/Contents/MacOS/JavaApplicationStub'
 alias hr='heroku restart'
-alias vi='mvim -v'
-alias vim='mvim -v'
