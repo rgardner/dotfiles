558d364cb0525e9b9bd0568259337d38c9caf161
Robert Gardner <rhg259@nyu.edu>
2015-02-18 11:24:04 -0500

    Move extreme/wallpaper out of dotfiles

--- bin/wallpaper.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-# wallpaper: cron script for downloading images from Reddit.
-/Users/bobgardner/.rvm/wrappers/ruby-2.1.1@alien-wallpaper/ruby \
-  /Users/bobgardner/Developer/alien-wallpaper/alien_wallpaper.rb \
-  -n 5 -o /Users/bobgardner/Pictures/Wallpaper/Reddit
