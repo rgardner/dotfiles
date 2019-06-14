731ba2366b7f9f84bf1c85a302ae6b1c3a03b059
Bob Gardner <bob.hn.gardner@gmail.com>
2019-06-13 21:36:31 -0700

    [grep] Remove GREP_OPTIONS auto

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -1,11 +1,11 @@
+#!/usr/bin/env bash
+
 # Case-insensitive globbing (used in pathname expansion)
 shopt -s nocaseglob
 
 # Check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
 shopt -s checkwinsize
-
-export GREP_OPTIONS='--color=auto'
 
 # Prevent less from clearing the screen while still showing colors.
 export LESS=-XR
